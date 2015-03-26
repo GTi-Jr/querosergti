334d6ec0cc722ceeb7bd35ea895f367e5af09d0d
Gui Carneiro <gui.carneiro15@gmail.com>
2015-03-26 17:35:03 -0300

    k

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,12 @@
 class Candidate < ActiveRecord::Base
   mount_uploader :resume, DocumentsUploader
   mount_uploader :history, DocumentsUploader
+
+
+  validates :email, :presence => true
+  validates :phone, :presence => true
+  validates :name, :presence => true
+  validates :register, :presence => true
+  validates :resume, :presence => true
+  validates :history, :presence => true
 end
