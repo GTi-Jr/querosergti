3b88cfcd61e50e0e2771efca86bd8304f2cfcb14
caioeps <caioergos@gmail.com>
2016-07-02 11:59:47 -0300

    sending emails through Gmail smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -35,7 +32,25 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :letter_opener
+  # MAILER SETTINGS BELOW
+  # Por padrão o Rails não envia emails em development.
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smpt
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PW'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  # END OF MAILER SETTINGS
 end
