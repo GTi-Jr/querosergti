15d7e97ec364396766bdccfae73f3d10936d76a7
caioeps <caioergos@gmail.com>
2016-07-02 15:59:55 -0300

    tradução dos erros para pt

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -4,24 +4,29 @@ class Candidate < ActiveRecord::Base
   mount_uploader :history, DocumentsUploader
 
   # Validations
-  validates :email, :presence => true
-  validates :phone, :presence => true
-  validates :name, :presence => true
-  validates :register, :presence => true
-  validates :resume, :presence => true
-  validates :history, :presence => true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :name, presence: true
+  validates :register, presence: true, length: { is: 6 }
+  validates :resume, presence: true
+  validates :history, presence: true
+  validate  :valid_email
 
   # Callbacks
   after_save :send_confirmation_email
-
-  scope :novatos, -> { select { register[0..1].to_i == Time.now.year - 1978 } }
 
   def first_name
     name.split(' ').first
   end
 
 private
+  # Callback methods
   def send_confirmation_email
     CandidateMailer.confirm_inscription(self).deliver
+  end
+
+  # Validation methods
+  def valid_email
+    errors.add(:email, 'deve ser válido') if email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   end
 end
